30c913f956efebdb74c93e277ada7cca2e6f181d
John mk UBUNTU <jmakateto@gmail.com>
2023-06-21 23:57:19 +0300

    completed assignement

--- index.js
+++ index.js
@@ -1,28 +1,23 @@
-// Write your code here!const element = document.createElement('div');
+const element = document.createElement("div");
 document.body.append(element);
 
-const ul = document.createElement('ul');
+const ul = document.createElement("ul");
 
 for (let i = 0; i < 3; i++) {
-  const li = document.createElement('li');
+  const li = document.createElement("li");
   li.textContent = (i + 1).toString();
   ul.append(li);
 }
 
 element.append(ul);
 
-const main = document.getElementById('main');
-main.innerHTML =
-  "<h1>Poodles!</h1><h3>An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a post-Frankfurt School Appropriationist Perspective</h3><p><em>By: Byron Q. Poodle, Esq., BA.</em></p>";
+const main = document.querySelector("main#main");
+main.innerHTML = "<h1>Poodles!</h1><h3>An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a post-Frankfurt School Appropriationist Perspective</h3><p><em>By: Byron Q. Poodle, Esq., BA.</em></p>";
 
-const element = document.getElementById('main');
-element.style.height = '300px';
-element.style.backgroundColor = '#27647B';
-element.textContent = "You've changed what's on the screen!";
-element.style.fontSize = '24px';
-element.style.marginLeft = '30px';
-element.style.lineHeight = 2;
+main.style.height = "300px";
+main.style.backgroundColor = "#27647B";
 
-const ul = document.getElementsByTagName('ul')[0];
-const secondChild = ul.querySelector('li:nth-child(2)');
-ul.removeChild
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "YOUR-NAME is the champion";
+document.body.appendChild(newHeader);
